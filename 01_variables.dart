36b0462fb9f91f7756470ed42d5b33662c63f3a4
Felipe Buitrago <flipe282000@gmail.com>
2023-10-27 09:41:11 -0500

    final, lists, null-aware operator

--- 01_variables.dart
+++ 01_variables.dart
@@ -1,4 +1,34 @@
 void main() {
-  const variableConstante = 20;
-  print(variableConstante);
+
+  const constvar = 20;
+
+  final String name = 'Felipe Buitrago';
+  final int healthPoint = 3333;
+  final bool isAlive = true;
+  final List<String> abilities = ['Coding', 'Sleeping'];
+
+  print("""
+    $constvar
+    $name
+    $healthPoint
+    $isAlive
+    $abilities
+  """);// multiline String """ string """
+
+
+  // Null safety
+  //int i = null; invalid in Dart
+  int? i = null; // anhadiendo ? al tipo, indica que puede ser null
+
+  // Null aware operator:
+  i ??= 33; // ??= es un operator que asignara el valor indicado si la variable es null
+  print(i);
+  i = null;
+
+  // Operador ?? evaluara el valor de la izquierda, siendo diferente a null, retornara este,
+  //sino evaluara el valor de la derecha para retornarlo si es diferente a null
+  print(null ?? 33);
+
+  int? nullable = i ?? 66;
+  print(nullable);
 }
