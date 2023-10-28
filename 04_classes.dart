c6eb1ff94f92d8ef9a235729748b3c1b44d4ef5d
Felipe Buitrago <flipe282000@gmail.com>
2023-10-28 13:51:13 -0500

    abstract classes and overriding

--- 04_classes.dart
+++ 04_classes.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
   
-  Person person1 = new Person(age: 23, name: 'Felipe');
+  final Person person1 = new Person(age: 23, name: 'Felipe');
   //Person person1 = Person(name: 'Felipe', age: 23); 'new' is not obligatory
   print(person1);
   print(person1.name);
@@ -13,19 +13,30 @@ class Person extends AbstractPerson{
   late String name;
   late int age;
 
-  Person({required String name, required int age}){
+  Person({ required String name, required int age} ){
     this.name = name;
     this.age  = age;
   }
-  /*
-  Person(String name, int age)
-   : this.name = name,
-     this.age  = age;
+
+  //Otras maneras de crear un constructor sin definir las propiedades como late
+
+  //Person(this.name, this.age);
   
-  */
+  //Person({ required this.name, required this.age} );
+
+  //Person(String name, int age)
+  // : this.name = name,
+  //   this.age  = age;
+  //
+
   @override
   String infoPerson(){
     return 'Hi, $name. You are $age years old.';
+  }
+
+  @override
+  String toString(){
+    return 'Modified toString.';
   }
 }
 
